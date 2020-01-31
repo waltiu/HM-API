fc5b05643b853d0f11ad818d94a68870f56f9805
waltiu <10470167@qq.com>
2020-01-31 09:17:44 +0800

    model

--- models/Village.js
+++ models/Village.js
@@ -3,25 +3,37 @@ const Village = mongoose.Schema;
 const VillageSchema = new Village({
   name: {
     type: String,
-    required: true
   },
-  password: {
+  area: {
     type: String,
-    required: true
-
   },
-  email: {
+  lng: {
     type: String,
-    required: true
-
+  },
+  lat: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  isSchoolOverArea: {
+    type: String,
+  },
+  district: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  isHosptialOverArea: {
+    type: String,
+  },
+  note: {
+    type: String,
   },
   time: {
     type: String,
     default: Date.now
-  },
-  picture: {
-    type: String,
-    default: 'https://www.baidu.com/link?url=przyPrQVfuVA9XF0L0T6uHmYLmFy7aaq_ubbC9394vsYGZBXxpCQJNctF7sZALe-mqfVBsir88qwjSmSfvO1Oz1bE3B-fQfjQ_QN_Dv58nBODXGUUjBpRP0UWSu3hCio0-a6OnV-HFfYfJOgdQakOZeZJWh3mB0q7SuU2mq74HAEV2xtS7t--qqhJjmF9DqH4frMGZoVh21ss9iONcnkwJYYHf8ifdTgNEU4NCKADQBr2ChciPcPutFDJsNpwxMIZ_heYaR3YtZ2jSh-0gILnxlB57xiWzJgfMKY5IbFNC9VVk7S7GB-seaU1iSiC4oHlyzb7yuyewMwWJ_jFhLh-NkEWGrlWT_VcTCd3DKllzya-ZTDMbilw5FHj4vEGUJzOtanw6_12Hgs-HHli6yfURgs6xp7uVQoD8YeSzxBx7b8D9Sl8FjaeChEV_VWdlLFZbcuUboF6Ru7KaKtwj4pcf5nkHhG7FSexJuGYIkxlZxd5EfRaHobjagRLHHNzTKpE6s9dC7CrQ2fMFvPwLJafNK7k0n06UYCFk9G-KDyuMMKozuaD3Q6tKyBgFShU71c-oDnpQTdxYxPDV_NAodDv-cgc2PnJIMWhK8cE0gzrcqx_PMukxbOV5RRnYXqrpaZ8QbP6fRt2CTWWAHHfwmXGq&timg=&click_t=1580373688455&s_info=1519_674&wd=&eqid=eca8c2dc0006bf27000000035e329467'
   }
 })
 
